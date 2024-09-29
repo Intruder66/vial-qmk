f84ae6b2a6887890de9942c43414bb265e54f508
Intruder66 <j.l.c.s@gmx.de>
2024-09-29 15:13:35 +0200

    Add Keychron K2 Pro ISO RGB VIA and VIAL keymaps

---
+++ keyboards/keychron/k2_pro/iso/rgb/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBA, 0x99, 0x8E, 0xD2, 0x4F, 0xD2, 0x64, 0x15}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
